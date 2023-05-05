46ebce759f0ae9f860d2bc4e510a294e06abb2d1
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-05 07:32:27 -0500

    added Contact page

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import Navigation from '../../components/Navigation/Navigation'
-import Header from '../../components/Header/Header'
+import Form from 'react-bootstrap/Form';
 
 export default function Contact() {
-
+  return (
+    <>
+    <div className="d-flex justify-content-center mt-5">
+    <p style={{}}>I can be reached via email at <a href="mailto:hoffmasterdevin@gmail.com">hoffmasterdevin@gmail.com</a>.</p>
+    <p style={{}}><a href="https://github.com/HoffmasterDevin">Github</a>.</p>
+    </div>
+    </>
+  );
 }
