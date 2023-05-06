cca39b217721ee9821b95c50b59b5b5813c39ab2
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-06 08:39:21 -0500

    added filler text

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -24,21 +24,21 @@ export default function AboutMe() {
           <Card.Text>
             12/2022 – Present<br />
             <em><b>Garde Manger, A Pig in a Fur Coat</b></em><br />
-            • <br />
-            • <br />
-            • <br />
-            • <br />
-            • <br /><br />
+            • Filler<br />
+            • Filler<br />
+            • Filler<br />
+            • Filler<br />
+            • Filler<br /><br />
             12/2021 – 12/2022<br />
             <em><b>Lead Line Cook, Graft</b></em><br />
-            • <br />
-            • <br />
-            • <br />
-            • <br /><br />
+            • Filler<br />
+            • Filler<br />
+            • Filler<br />
+            • Filler<br /><br />
             <em><b>Kitchen Manager, Red Sushi</b></em><br />
-            • <br />
-            • <br />
-            • <br /><br />
+            • Filler<br />
+            • Filler<br />
+            • Filler<br /><br />
             <em><b>EDUCATION</b></em><br />
             MAY 2023 <br />
             <em><b>Full Stack Web Development, University of Wisconsin Madison Boot Camp</b></em><br />
